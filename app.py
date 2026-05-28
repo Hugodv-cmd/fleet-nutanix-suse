62b138606f246cd4fc0ca936eef809f696f23cdd
Hugodv-cmd <hugo.devries@gmail.com>
2026-05-28 11:14:30 +0200

    Update app.py
    changed label from clustername to hypervisor

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # Fleet will dynamically inject this environment variable
-    cluster_name = os.getenv('CLUSTER_NAME', 'Unknown Kubernetes Cluster')
-    return f"Hello World! This application is being served from: {cluster_name}\n"
+    hypervisor = os.getenv('HYPERVISOR', 'Unknown Hypervisor')
+    return f"Hello World! This application is being served from: {hypervisor}\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
