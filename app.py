a0937162b57dd73da86bafef6ff7fd6e25c705c1
Hugodv-cmd <hugo.devries@gmail.com>
2026-05-28 11:36:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     # Fleet will dynamically inject this environment variable
     hypervisor = os.getenv('HYPERVISOR', 'Unknown Hypervisor')
-    return f"Hello World! This application is being served from: {hypervisor}\n"
+    return f"Hello World! This application is being served from {hypervisor}\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
